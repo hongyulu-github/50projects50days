cf0e3508df1b5481d74cea3f629338cf7ae3a738
luluengithub <43068006+luluengithub@users.noreply.github.com>
2023-04-30 03:53:41 +0200

    Add files via upload

---
+++ D04 Hidden Search/app.js
@@ -0,0 +1,9 @@
+const searchBTN = document.getElementById("search-btn") 
+const input = document.querySelector(".input")
+
+searchBTN.addEventListener("click",()=>{
+
+ input.classList.toggle("active");
+ //input.focus()
+ 
+})
